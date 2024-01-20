6974719c12ebac3bd44e1eef3238670a03a9bfb0
kruzelgo <kruzelgo@gmail.com>
2024-01-20 18:38:59 +0100

    7,8,9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginFormElement = document.querySelector(".login-form");
+loginFormElement.addEventListener("submit", handleLoginSubmit);
 
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector(".change-color");
+const body = document.body;
+const color = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  color.innerText = newColor;
+});
