b308120f5fa83cd855fd572386b38af24a5cc7af
kruzelgo <kruzelgo@gmail.com>
2024-01-20 13:05:45 +0100

    task 2 and 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
+const ingredientsList = document.querySelector("#ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// const ingredientsList = document.createElement("ingredients");
+console.log(ingredientsList);
+
+ingredients.forEach((ingredient) => {
+  const listElement = document.createElement("li");
+
+  listElement.textContent = ingredient;
+
+  listElement.classList.add("item");
+
+  ingredientsList.append(listElement);
+});
